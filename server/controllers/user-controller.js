340a5efaf886853e77b4db7f7ae68c1ef4573672
Brent <brentg123@gmail.com>
2023-05-17 17:09:39 -0500

    Added user-controller code

---
+++ server/controllers/user-controller.js
@@ -0,0 +1,31 @@
+const { user, client } = require('../server');
+
+const getUser = (req, res) => {
+  if (user != {}) {
+      res.status(200).json(user);
+  }
+  else {
+    res.status(400).json({ message: "User not found."})
+  }
+}
+
+const sendTweet = async (req, res) => {
+  const tweet = req.body.tweet;
+
+  // Add try/catch block
+  try{
+    if (tweet.length == 0) {
+      res.status(401).json({ message: "Text field is empty. Please write something to tweet."});
+    }
+    // need to check if user has Twitter Blue. If so, max limit should be 4000 characters
+    else if (tweet.length > 280) {
+      res.status(401).json({ message: "There are too many characters in your tweet. Cannnot post message." })
+    }
+    else {
+      const result = await client.post('statuses/update', { status: req.body.tweet });
+      res.status(200).json({ message: `You successfully tweeted this : ${result.text}` });
+    }
+  } catch {
+    res.status(500).json({ message: "Failed to send tweet." });
+  }
+}
--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -1,21 +1,11 @@
-app.get('/user', (req, res) => {
-  res.send(user)
-})
+const router = require("express").Router();
+const {
+  createUser,
+  signoutUser,
+  deleteUser,
+  authenticateLogin,
+  authenticateStatus,
+  changePassword
+} = require('../../controllers/user-controller');
 
-app.post('/tweet', (req, res) => {
-  res.sendStatus(200)
-  const tweet = req.body.tweet
-  
-  if (tweet.length == 0) {
-    console.error('Text field is empty - there is nothing to tweet!')
-  }
-  else if (tweet.length > 280) {
-    console.error('There are too many characters in your tweet - cannnot post message!')
-  }
-  else {
-    client.post('statuses/update', { status: req.body.tweet }).then(result => 
-      {console.log('You successfully tweeted this : "' + result.text + '"')})
-      .catch(console.error)
-  }
-}
-)
+module.exports = router;
