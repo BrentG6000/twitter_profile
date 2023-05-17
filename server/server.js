058b928a0a6c5e7af10b950b32890f0c071f6c25
Brent <brentg123@gmail.com>
2023-05-17 14:37:03 -0500

    Refactored server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,41 @@
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const passport = require('passport')
-const session = require('express-session')
-const twitter = require('twitter-lite')
-const TwitterStrategy = require('passport-twitter').Strategy
-const { initialize } = require('passport')
+const express = require('express');
+const path = require('path');
+const passport = require('passport');
+const session = require('express-session');
+const twitter = require('twitter-lite');
+const TwitterStrategy = require('passport-twitter').Strategy;
+const routes = require('./routes');
 
-const consumerKey = process.env.CONSUMER_KEY
-const consumerSecret = process.env.CONSUMER_SECRET
+const PORT = process.env.PORT || 3001;
+const environment = process.env.Node_ENV;
+const consumerKey = process.env.CONSUMER_KEY;
+const consumerSecret = process.env.CONSUMER_SECRET;
 
-let user = {}
+let user = {};
 let liteArgs = {  
   consumer_key: consumerKey ,  
   consumer_secret: consumerSecret,  
   access_token_key: '',  
   access_token_secret: ''  
+};
+let client;
+
+const app = express();
+
+// Parses requests that include URL
+app.use(express.urlencoded({ extended: true }));
+
+// Parses requests that include JSON
+app.use(express.json());
+
+/* If we're in production, this allows Express to serve the static assets from /client/build. When
+the project is built, React places its static assets (JavaScript, CSS, Images, etc.) there in chunk
+files for efficiency, so using this directory in production is also more efficent. */
+if (environment === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
 }
-let client
-const PORT = process.env.PORT || 5000
+
+app.use(routes);
 
 passport.use(new TwitterStrategy({
   consumerKey: consumerKey,
@@ -35,53 +52,16 @@ function(token, tokenSecret, profile, cb) {
 
 passport.serializeUser((user, cb) => {
   cb(null, user)
-})
+});
 
 passport.deserializeUser((obj,cb) => {
   cb(null, obj)
-})
-
-const app = express()
-
-app.use(cors())
-app.use(express.static(path.resolve(__dirname, '../client/build')))
-app.use(express.urlencoded({ extended: true }))
-app.use(session({
-  secret: "PmX'tK11lc$78szv#fUc\"(|(-Q8lFGWTu<tgL*n}TD[", resave: true, saveUninitialized: true, secure: true }))
-app.use(passport.initialize())
-app.use(passport.session())
-app.use(express.json())
-
-app.get('/auth/twitter', passport.authenticate('twitter'))
-app.get('/auth/twitter/callback', 
-  passport.authenticate('twitter', { failureRedirect: '/login' }),
-  function(req, res) {
-      //res.redirect('http://localhost:3000/Profile')
-      res.redirect('https://brentg123-twitter-project.herokuapp.com/Profile')
-  })
-app.get('/user', (req, res) => {
-    res.send(user)
-})
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
 
-app.post('/tweet', (req, res) => {
-  res.sendStatus(200)
-  const tweet = req.body.tweet
-  
-  if (tweet.length == 0) {
-    console.error('Text field is empty - there is nothing to tweet!')
-  }
-  else if (tweet.length > 280) {
-    console.error('There are too many characters in your tweet - cannont post message!')
-  }
-  else {
-    client.post('statuses/update', { status: req.body.tweet }).then(result => 
-      {console.log('You successfully tweeted this : "' + result.text + '"')})
-      .catch(console.error)
-  }
-}
-)
+app.use(session({
+  secret: "PmX'tK11lc$78szv#fUc\"(|(-Q8lFGWTu<tgL*n}TD[", resave: true, saveUninitialized: true, secure: true }));
 
-app.listen(PORT, () => console.log('Server is running'))
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
