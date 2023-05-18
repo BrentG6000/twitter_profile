b2c35c8c52cfdb921c169ec9e48b7704e6cb7ed4
Brent <brentg123@gmail.com>
2023-05-18 13:22:28 -0500

    Removed catch all route

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,4 @@ const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
-});
-
 module.exports = router;
